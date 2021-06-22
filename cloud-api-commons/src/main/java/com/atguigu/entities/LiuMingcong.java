d6ce03f06cbec205b2589625dffd77f995dc0abe
lmc <320692934@qq.com>
2021-06-22 17:31:36 +0800

    author's introduce

--- cloud-api-commons/src/main/java/com/atguigu/entities/LiuMingcong.java
+++ cloud-api-commons/src/main/java/com/atguigu/entities/LiuMingcong.java
@@ -1,5 +1,7 @@
 package com.atguigu.entities;
 
+import java.util.List;
+
 /**
  * 2 * @Author: Liu Mingcong
  * 3 * @Date: 2021/6/22 17:08
@@ -8,4 +10,5 @@ package com.atguigu.entities;
 public class LiuMingcong {
     private static final String name = "刘銘聪";
     private static final String sex = "男";
+    private List<String> girlFriends;
 }
