6c3f74d7de837cab682f436a6a9156dd505942cf
lmc <320692934@qq.com>
2021-06-22 17:52:29 +0800

    author's introduce

--- cloud-api-commons/src/main/java/com/atguigu/entities/LiuMingcong.java
+++ cloud-api-commons/src/main/java/com/atguigu/entities/LiuMingcong.java
@@ -11,4 +11,14 @@ public class LiuMingcong {
     private static final String name = "刘銘聪";
     private static final String sex = "男";
     private List<String> girlFriends;
+
+    public List<String> getGirlFriends() {
+        for (String entry : girlFriends) {
+            synchronized (this) {
+
+                return girlFriends;
+            }
+        }
+        return null;
+    }
 }
