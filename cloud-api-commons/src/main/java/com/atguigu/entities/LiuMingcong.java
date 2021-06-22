c8789b1709397d69bde6449d504752a9b89ea32e
lmc <320692934@qq.com>
2021-06-22 17:14:37 +0800

    野鸡无名

---
+++ cloud-api-commons/src/main/java/com/atguigu/entities/LiuMingcong.java
@@ -0,0 +1,11 @@
+package com.atguigu.entities;
+
+/**
+ * 2 * @Author: Liu Mingcong
+ * 3 * @Date: 2021/6/22 17:08
+ * 4
+ */
+public class LiuMingcong {
+    private static final String name = "刘銘聪";
+    private static final String sex = "男";
+}
